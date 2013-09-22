4c4e83f36c4b50ece63e25f94bfc0a49dcd101b9
carlos <scadtonia@gmail.com>
2013-09-22 18:13:15 -0500

    Initial Commit

---
+++ Quantus/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Quantus
+//
+//  Created by Carlos Uribe on 22/09/13.
+//  Copyright (c) 2013 Comunicacion Constructiva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quantus/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Quantus
+//
+//  Created by Carlos Uribe on 22/09/13.
+//  Copyright (c) 2013 Comunicacion Constructiva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
